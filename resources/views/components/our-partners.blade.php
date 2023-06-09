f52f153a8c2198e6a02739cfc4d27e46850b11bf
emmymayo <odudareemmanuel@gmail.com>
2023-06-09 09:25:38 +0100

    started partner

---
+++ resources/views/components/our-partners.blade.php
@@ -0,0 +1,22 @@
+<section class="bg-white my-18"  
+    data-aos="fade-left"  
+    data-aos-delay="200"
+    data-aos-duration="600" 
+    data-aos-easing="ease-in-sine"
+>
+    <div class="flex">
+        <div class="grid">
+            <img src="" alt="">
+            <p>Chisaacs Signature</p>
+        </div>
+        <div class="grid">
+            <img src="" alt="">
+            <p>Oakyoung Associates</p>
+        </div>
+        <div class="grid">
+            <img src="" alt="">
+            <p>Oakyoung Credits</p>
+        </div>
+        <div class="grid"></div>
+    </div>
+</section>
